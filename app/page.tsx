273c0e62d067b5a2f6c4ab4a8056a7e902c2781a
Harshal Khandare <harshalkhandare153@gmail.com>
2026-06-30 11:29:58 +0530

    Initial project setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>🏡 Plot Selling Website</h1>
+      <p>Find the best plots at best prices</p>
+
+      <button>View Projects</button>
+    </div>
+  );
+}
